dc6f49c6dd067ddfd4440e87c6c2120323cd1310
GuapoTaco <gouseyahoo@gmail.com>
2014-11-09 19:59:26 -0700

    RE-Add character ETC
    Open this in eclipse by setting the workspace to /RPG-Simulator/ Then
    create project RPG

---
+++ RPG/src/main/Character.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.Vector;
+
+// base character class. Can be used by either enemy or friendly
+public class Character {
+	
+	 enum BodyParts{
+		torso,
+		leg,
+		arm,
+		head
+	 }
+	
+	float currentHP;
+	float maxHP;
+	float attackStrength;
+	Vector<BodyParts> bodyParts;
+}
