20128dc567e7c28fb57a01ae61bbcd7fc5500b07
GuapoTaco <russellgreene8@gmail.com>
2014-11-11 16:26:46 -0700

    Make pausing
    Pressing the esc button pauses the game

--- RPG/src/main/Game.java
+++ RPG/src/main/Game.java
@@ -9,20 +9,22 @@ import java.util.Vector;
 public class Game extends StateBasedGame {
 
 	static final int play = 0;
+	static final int pause = 1;
 	public static float GameTotalTime = 0;
-	public static Vector<Object> TickingObjects;
+	public static Vector<Object> TickingObjects = new Vector<Object>();
 	
 	
 	public Game(String name) {
 		super(name);
 		
 		addState(new Play(play));
-		
+		addState(new Pause(pause));
 	}
 
 	@Override
 	public void initStatesList(GameContainer gc) throws SlickException {
 		getState(play).init(gc, this);
+		getState(pause).init(gc, this);
 		enterState(play);
 		
 	}
--- RPG/src/main/Pause.java
+++ RPG/src/main/Pause.java
@@ -3,59 +3,47 @@ package main;
 import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class Play extends BasicGameState {
+public class Pause extends BasicGameState {
 
+	boolean isESCStillDown = false;
+	
 	public final int state;
 	
-	public Play(int statein){
-		state=statein;
+	
+	public Pause(int stateID){
+		state = stateID;
 	}
 	
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
-
-
 	}
 
 	@Override
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics gr)
+	public void render(GameContainer gc, StateBasedGame sbf, Graphics gr)
 			throws SlickException {
-		Input i = gc.getInput();
-		
+
+		gr.drawString("Paused", 200, 200);
 		gr.drawString(Float.toString(Game.GameTotalTime), 100, 100);
-		
 	}
 
 	@Override
-	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+	public void update(GameContainer gc, StateBasedGame sbg, int deltaTime)
 			throws SlickException {
-		
-		Game.GameTotalTime += delta;
-	
-		
-		for(int i = 0; i < Game.TickingObjects.size(); i++){
-			Game.TickingObjects.get(i).tick(delta);
+
+		if(gc.getInput().isKeyPressed(Keyboard.KEY_ESCAPE)){
+			sbg.enterState(Game.play);
 		}
 		
-		Input i = gc.getInput();
-		
-		if(i.isKeyPressed(Keyboard.KEY_ESCAPE)){
-			sbg.enterState(Game.pause);
-		}
 	}
 
 	@Override
 	public int getID() {
-		
 		return state;
 	}
-	
-
 
 }
