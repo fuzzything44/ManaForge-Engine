57fc1c60d4f4eb16b2771edc13a17496d2db6d8a
TheMadArtificer <5k.dylank@gmail.com>
2014-11-12 15:03:34 -0700

    Update ManaPotion.java

--- RPG/src/items/ManaPotion.java
+++ RPG/src/items/ManaPotion.java
@@ -8,6 +8,10 @@ public class ManaPotion extends ConsumableItem {
 	public void use() {
 		super.use();	// Is this correct? We want to override use().
 		System.out.println("It has a delivious, fruity taste to it. Additionally, you die.");
+		itemAmount--; 
+		if (itemAmount <= 0) {
+			//Add stuff for having none left
+		}
 	}
 
 }
