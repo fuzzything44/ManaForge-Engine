1e55aa26929de5fe23a6a420cc0023011328bd2a
GuapoTaco <gouseyahoo@gmail.com>
2014-11-17 17:44:52 -0700

    Add basic functionality to actor class

--- RPG/src/main/Actor.java
+++ RPG/src/main/Actor.java
@@ -1,5 +1,25 @@
 package main;
 
-public class Actor extends Object {
+/* The base class for anything placeable in the world
+ * 
+ */
 
+
+
+public class Actor extends Object {
+	
+	public  Coordinate getLocation(){
+		return Location;
+	}
+	
+	public void Teleport(Coordinate newLocation){
+		Location = newLocation;
+		
+		// TODO: Add teleport handling stuff
+	}
+	
+	 
+	
+	
+	private Coordinate Location;
 }
