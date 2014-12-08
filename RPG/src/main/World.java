13ea64b34ea0be420713f5213d638fb6911202af
fuzzything44 <slimesftw@gmail.com>
2014-12-07 19:01:56 -0700

    Working on world loading from a file.
    Not much else.

--- RPG/src/main/World.java
+++ RPG/src/main/World.java
@@ -1,33 +1,58 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class World {
 	
-	public Coordinate ChunkRes = new Coordinate(50, 50);
+	public Coordinate ChunkRes;
 	
 //	public Map<Coordinate, WorldChunk> chunks = new HashMap<Coordinate, WorldChunk>();
 
-	public WorldChunk[][] chunks = new WorldChunk[10][10];
+	public WorldChunk[][] chunks;
 	public WorldChunk persistentChunk = new WorldChunk(new Coordinate(0, 0), this);
 
 	public void load(String file) {
-		// Method to load world from a file.
-		for (int X = 0; X < 10; X++) {
-			for (int Y = 0; Y < 10; Y++) {
-				new WorldChunk(new Coordinate(X, Y), this);
-			}	// End Y for
-		}		// End X for
-		// Temporary chunk generation.
-		for (int chunkX = 0; chunkX < Game.world.chunks.length; chunkX++) {
-			for (int chunkY = 0; chunkY < Game.world.chunks[0].length; chunkY++) {
-				for (int actorX = (int) -Game.world.ChunkRes.X; actorX < (int) Game.world.ChunkRes.X; actorX++) {
-					for (int actorY = (int) -Game.world.ChunkRes.X; actorY < (int) Game.world.ChunkRes.Y; actorY++) {
-						Game.world.chunks[chunkX][chunkY];	// TODO fix WorldChunk actors, fix basic generation.
-					}
+		// TODO Get the correct file. Needs to work on all builds.
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file) );
+			while (reader.ready()) {	// As long as there's another line...
+				String line = reader.readLine();
+				
+				// Most lines will be in the form of TYPE_CoordX_CoordY_(Other data)
+				
+				String[] splitLine = line.split("_");
+				if (splitLine[0].equals("C") ) {
+					// Type is chunk.
+					
+				} else if (splitLine[0].equals("A") ) {
+					
+				} else {
+					ChunkRes.X = Float.parseFloat(splitLine[0]);
+					ChunkRes.Y = Float.parseFloat(splitLine[1]);
+					
+					chunks = new WorldChunk[Integer.parseInt(splitLine[2])][Integer.parseInt(splitLine[3])];
+					
+					// Should be the first line of a world. Should be in the form of
+					// ChunkResX_ChunkResY_NumChunksX_NumChunksY
 				}
-			}
+				
+			}	// End while
+
+			reader.close();
+			// Closing reader to stop memory leaks.
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();	// TODO get some exception handling here.
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
 		}
+		
+		
 	}			// End load()
 }
