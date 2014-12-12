7794f3af1d791de919851dbb8e42c9ae7f30ea98
fuzzything44 <slimesftw@gmail.com>
2014-12-12 16:52:19 -0700

    Bit more macro work.

--- RPG/src/main/Macros.java
+++ RPG/src/main/Macros.java
@@ -1,5 +1,6 @@
 package main;
 
+import buffs.Buff;
 import buffs.Buff.BuffValues;
 import items.ConsumableItem;
 import items.Equipment;
@@ -11,11 +12,25 @@ import items.Equipment;
 public class Macros {
 
 	public static Actor actor (String actorType, Coordinate location) {
-		Actor generatedActor = new Actor(0, new WorldChunk(new Coordinate(0, 0), Game.world)); //TODO: actually parse it !!
+		WorldChunk actorChunk = Game.world.getActorChunk(location);
+		String actorImage = "res/Default.png";
+		int actorRender = 0;
+		// Variables needed to create an actor.
+		Actor generatedActor = null;
 		if (actorType.equalsIgnoreCase("grass")) {
-			//generatedActor = new Actor(0, );
+			actorImage = "res/grass.png";
+			// Grass is a background actor, and therefore is rendered first.
+		} else if (actorType.equalsIgnoreCase("tree")) {
+			generatedActor = new Actor(1, actorChunk, "res/tree.png", location);
+			// Trees have a render order of 1. Currently.
+			// Trees also need to be solid.
+			
 		} else {
 			
+		}
+
+		if (generatedActor == null) {
+			generatedActor = new Actor(actorRender, actorChunk, actorImage, location);
 		}
 		
 		return generatedActor;
@@ -25,8 +40,9 @@ public class Macros {
 		ConsumableItem generatedItem = null;
 		
 		
+		
 		return generatedItem;
-	}
+	}	// End consumable generation.
 	
 	public static Equipment equipment (String equipName) {
 		Equipment generatedEquipment = null;
@@ -41,17 +57,18 @@ public class Macros {
 		return generatedEquipment;
 	}	// End equipment
 	 
-	public static BuffValues buff (String buffName) {
+	public static Buff buff(String buffName) {
 		BuffValues generatedBuff = new BuffValues();
+		String generatedName = "Buff";
+		// Basic buff set to do nothing and be named "Buff"
+		
 		if (buffName.equalsIgnoreCase("attack_small")) {
 			generatedBuff.attackBuffNumber = 5;
 		} else if (buffName.equalsIgnoreCase("megaBuff")) {
 			
 		}
 		
-		
-		
-		
-		return generatedBuff;
-	}
+		return new Buff(generatedName, generatedBuff);
+	}	// End buff generation
+	
 }
