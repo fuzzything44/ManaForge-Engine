37a2609a75555c8a7d5575b83492a430c4d9c63f
fuzzything44 <slimesftw@gmail.com>
2014-12-19 15:50:37 -0700

    More on logs
    More logging crashes. Yay!

--- RPG/src/main/LogCreator.java
+++ RPG/src/main/LogCreator.java
@@ -10,17 +10,18 @@ public class LogCreator {
 // A class for creating crash logs and such.
 // crashLog takes the exception and Play instance. If no play instance, it can take just the exception
 	private LogCreator() {}
-	public static void crashLog(Exception e) {
-		crashLog(e, null);
-	}
 	
-	public static void crashLog(Exception e, Play p) {
+	public static String logLocation = "";
+	// String for where the log should be generated. Ex. C:/folder/anotherfolder/
+
+	// crashLog makes logs on crashes. Has exception
+	public static void crashLog(Exception e) {
 		try {
 			String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(Calendar.getInstance().getTime());			
 			// Gets the current time as a String. Example:
 			// 2014-12-18_15-58-26
 			// Comes from running it at December 18, 2014 at 3:58 PM. 26 seconds after that time.
-			String logName = "log-" + timeStamp + ".txt";
+			String logName = logLocation + "crash-" + timeStamp + ".txt";
 			
 			BufferedWriter logFile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(logName) ) );
 			// Writer to create the log file
@@ -40,25 +41,6 @@ public class LogCreator {
 			}
 			// Writing the entire stack trace.
 			
-			if (p != null) {
-				logFile.write("Chunks loaded: " + p.relevantChunks.size() );
-				logFile.newLine();
-				// Writing how many chunks are loaded.
-				
-				int actorsLoaded = 0;
-				for (int i = 0; i < p.relevantActors.size(); i++) {
-					actorsLoaded += p.relevantActors.get(i).size();
-				}
-				logFile.write("Actors loaded: " + actorsLoaded);
-				// Calculating and writing how many actors are loaded
-				
-			} else {
-				// If no Play instance was given, say so.
-				logFile.write("Nonstandard error - no Play loaded");
-			}
-			
-			logFile.newLine();
-			
 			// Anything else we want to have in log files?
 			logFile.close();
 		} catch (Exception fail) {
@@ -66,5 +48,29 @@ public class LogCreator {
 		}
 
 	}	// End crashLog
+	
+	public static void performanceLog(Play p) {
+		try {
+			String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(Calendar.getInstance().getTime());			
+			// Gets the current time as a String. Example:
+			// 2014-12-18_15-58-26
+			// Comes from running it at December 18, 2014 at 3:58 PM. 26 seconds after that time.
+			String logName = logLocation + "performance-" + timeStamp + ".txt";
+
+			BufferedWriter logFile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(logName) ) );
+			// Writer to create the log file
+			
+			logFile.write("Performance log:");
+			logFile.newLine();
+			logFile.write("Chunks: " + p.relevantChunks.size());
+			logFile.newLine();
+			// More on performance logs here. We should figure out what we all want.
+			
+			
+			logFile.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
+	}	// End performanceLog
 }
