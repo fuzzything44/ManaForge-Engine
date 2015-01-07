61b7bf8237afd2f161b27f8f6302b13337a223e4
fuzzything44 <slimesftw@gmail.com>
2015-01-07 15:34:25 -0700

    More work on World Editor

--- World Editor/src/main/World.java
+++ World Editor/src/main/World.java
@@ -1,6 +1,13 @@
 package main;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Image;
+
 
 public class World {
 	Map<Coordinate, Image> backgroundImages = new HashMap<Coordinate, Image>();
@@ -15,6 +22,15 @@ public class World {
 	}
 	
 	public void save(String folderName) {
+		Set<Color> usedColors = new HashSet<Color>();
+		
+		// Create an image here.
+		Image image = null;
+		for (int x = minX; x < maxX; x++) {
+			for (int y = minY; y < maxY; y++) {
+				// Add actor to image color.
+			}
+		}
 		
 	}
 }
