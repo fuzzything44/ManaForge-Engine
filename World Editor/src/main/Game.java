e5e9cdc6dea63868a5f6494f8bb656d63a3ebec6
fuzzything44 <slimesftw@gmail.com>
2015-01-07 15:26:15 -0700

    World Editor start
    Should have a decent amount of code reuse.

---
+++ World Editor/src/main/Game.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Game {
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+} 
---
+++ World Editor/src/main/World.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.HashMap;
+
+public class World {
+	Map<Coordinate, Image> backgroundImages = new HashMap<Coordinate, Image>();
+	
+	int minX = 0;
+	int maxX = 0;
+	int minY = 0;
+	int maxY = 0;
+	
+	public World() {
+		
+	}
+	
+	public void save(String folderName) {
+		
+	}
+}
