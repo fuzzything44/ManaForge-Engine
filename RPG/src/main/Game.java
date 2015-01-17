c89ef7682edb22ee12ccfad3b377480a20d5bcb8
GuapoTaco <russellgreene8@gmail.com>
2015-01-17 15:59:44 -0700

    lwjgl opencl in real stuff

--- RPG/src/main/Game.java
+++ RPG/src/main/Game.java
@@ -6,17 +6,6 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
-import com.nativelibs4java.opencl.CLContext;
-import com.nativelibs4java.opencl.CLKernel;
-import com.nativelibs4java.opencl.CLProgram;
-import com.nativelibs4java.opencl.CLQueue;
-import com.nativelibs4java.opencl.JavaCL;
-import com.nativelibs4java.util.IOUtils;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.ByteOrder;
 import java.util.Map;
 
 public class Game extends StateBasedGame {
@@ -33,12 +22,6 @@ public class Game extends StateBasedGame {
 	public static int zoom = 48;
 	static Map<String, Image> textures;
 	
-	public static CLContext context;
-	public static CLQueue queue;
-	public static ByteOrder byteOrder;
-	public static CLProgram program;
-    public static CLKernel collideKern;
-
 	
 	public Game(String name) {
 		super(name);
@@ -51,33 +34,7 @@ public class Game extends StateBasedGame {
 	public void initStatesList(GameContainer gc) throws SlickException {
 
 	    
-		try {
-			
-			context = JavaCL.createBestContext();
-		    queue = context.createDefaultQueue();
-		    byteOrder = context.getByteOrder();
-		    
-		    Reader reader = new FileReader("src/kernels/Collision.cl");
-		    String src = new String();
-		    
-		    int data = reader.read();
-		    while(data != -1){
-		    	src += (char)(data);
-		        data = reader.read();
-		    }
-	        
-	        program = context.createProgram(src);
-	        
-	        collideKern = program.createKernel("collide");
-	        
-	        reader.close();
-	        
-		} catch (IOException e) {
-			e.printStackTrace();
-			LogCreator.crashLog(e);
-			System.exit(1);
-		}
-		
+
 		getState(pause).init(gc, this);
 		enterState(play);
 		
