741ce65544011ef08c6941b98f1cd262b24cc460
Russell Greene <russellgreene8@gmail.com>
2015-01-28 21:00:28 -0700

    add some more comments for you:)

--- RPG-Game/MainWindow.h
+++ RPG-Game/MainWindow.h
@@ -5,12 +5,15 @@
 #include <GLTools/glew.h>
 #include <GLTools/glfw3.h>
 
+// Handler class for window
 class MainWindow
 {
 public:
 
+	// Run the window. Consumes the thread. 
 	static GLint MainWindow::run(GLuint width, GLuint height, const GLchar* title, GLFWmonitor* monitor, GLFWwindow* share);
 
+	// Function binding functions
 	static GLvoid bindDraw(GLvoid(*drawIn)(GLfloat));
 	static GLvoid bindKeyboard(GLvoid(*keyboradIn)(GLFWwindow*, GLfloat));
 	static GLvoid bindInit(GLint(*initIn)());
@@ -23,10 +26,11 @@ public:
 	static GLFWwindow* window;
 
 private:
+	
+	// function pointers
 	static GLvoid(*draw)(GLfloat);
 	static GLvoid(*keyboard)(GLFWwindow* window, GLfloat);
 	static GLint(*init)();
 	static GLint(*exit)();
-	
 	static GLvoid(*scroll)(GLFWwindow*, GLdouble, GLdouble);
 };
