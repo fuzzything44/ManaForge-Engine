a68d8512e259b930c278fed34bde251365eb3b09
Russell Greene <russellgreene8@gmail.com>
2015-02-01 15:00:51 -0700

    Add DDS loading methods to Helper

--- RPG-Game/Helper.h
+++ RPG-Game/Helper.h
@@ -16,6 +16,11 @@
 #include <vector>
 
 
+#define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1" in ASCII
+#define FOURCC_DXT3 0x33545844 // Equivalent to "DXT3" in ASCII
+#define FOURCC_DXT5 0x35545844 // Equivalent to "DXT5" in ASCII
+
+
 /// <summary> decodes an image and saves it to the image vector. </summary>
 /// <param name='image'> A reference to a vector of GLubyte (unsigned char). Must be initalized. </param>
 /// <param name='filename'> The path to the image. Must be a PNG file. </param>
@@ -71,6 +76,22 @@ GLuint loadTexture(const GLchar* filepath, GLuint& width, GLuint& height);
 /// <param name='err'> A cl_int* that is set to the error code. Suggested for error handling. </param>
 cl::Program loadCLProgram(const GLchar* filepath, cl::Context& context, std::vector<cl::Device>& devices, cl_int* err = NULL);
 
+/// <summary> load a DDS format texture to the GLuint that is returned </summary>
+/// <param name='imagepath'> the pathe to the DDS file </param>
+GLuint loadDDS(const GLchar * imagepath);
+
+/// <summary> load a DSS format to the texture specified at the location specified </summary>
+/// <param name='texToAppend'> The handle for the texture to append the image to </param>
+/// <param name='Xoffset'> The X offset of the original texture to place the image </param>
+/// <param name='Yoffset'> The Y offset of the original texture to place the image </param>
+/// <param name='filepath'> The path to the DDS file. </param>
+void appendDDS(GLuint texToAppend, GLuint Xoffset, GLuint Yoffset, const GLchar* filepath);
+
+/// <summary> allocates an empty texture library from the format of the given DDS file </summary>
+/// <param name='num'> The amount of textures in one direction. total textures is up to num squared </param>
+/// <param name='filepath'> The path to the DDS file to load </param>
+GLuint allocateCompressedTextureLibraryFromDDS(GLuint num, const char* filepath);
+
 /// <summary> OpenCL err checking. Returns true is successfull (CL_SUCCESS) </summary>
 /// <param name='error'> The error code. </param>
 /// <param name='name'> The name of the operation. Used for debugging later. </param>
@@ -85,6 +106,5 @@ inline cl_bool errChkCL(cl_int error, const char* name)
 	{
 		return true;
 	}
-
 
 }
