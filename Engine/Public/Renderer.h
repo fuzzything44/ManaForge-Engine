342af37d617c780ea737fbba2380be00589c0789
GuapoTaco <russellgreene8@gmail.com>
2015-04-05 15:46:58 -0400

    add lodepng

--- Engine/Public/Renderer.h
+++ Engine/Public/Renderer.h
@@ -3,6 +3,7 @@
 #include "Engine.h"
 #include "Actor.h"
 #include <Transform.h>
+#include "Camera.h"
 
 class Renderer 
 {
@@ -30,17 +31,13 @@ public:
 		//virtual void addRelativeScale(vec2 scaleToAdd) = 0;
 
 
-	private:
-
-		Transform transform;
-
 	};
 
 	/// <summary> Default constructor.</summary>
 	Renderer() {};
 	
 	/// <summary>
-	virtual void init() = 0;
+	virtual void init(std::string textures) = 0;
 	
 	/// <summary> Renders the next frame. </summary>
 	virtual void update() = 0;
@@ -50,12 +47,14 @@ public:
 	/// <param name="newCamera"> The camera it should render at. </param>
 	virtual void setCamera(Camera* newCamera) = 0;
 
+	virtual Camera*  getCamera() = 0;
+
 	/// <summary> Creates a new model.</summary>
 	///
 	/// <returns> null if it fails, else a Model*.</returns>
 	virtual Model* newModel(float* locations, float* UVs, int numVerts, int numElems) = 0;
 
-
+	
 
 	virtual ~Renderer() { }
 
--- OpenGLRenderer/OpenGLRenderer.cpp
+++ OpenGLRenderer/OpenGLRenderer.cpp
@@ -6,7 +6,7 @@
 #include <Actor.h>
 
 class OpenGLRenderer : public Renderer
-{ 
+{
 public:
 
 	class OpenGLModel : public Model
@@ -38,15 +38,53 @@ public:
 
 	virtual Model* newModel(float* locations, float* UVs, int numVerts, int numElems) override;
 
+	/// <summary>
+	virtual void init(std::string textures) override;
+
+	/// <summary> Renders the next frame. </summary>
+	virtual void update() override;
+
+	/// <summary> Sets camera to render at. </summary>
+	///
+	/// <param name="newCamera"> The camera it should render at. </param>
+	virtual void setCamera(Camera* newCamera) override;
+
+
+	Camera* getCamera() override;
+
 	OpenGLRenderer() { }
 
-private:
 
+
+private:
+	Camera* currentCamera;
 };
 
 Renderer::Model* OpenGLRenderer::newModel(float* locations, float* UVs, int numVerts, int numElems)
 {
 	return new OpenGLModel(locations, UVs, numVerts, numElems);
+}
+
+void OpenGLRenderer::init(std::string textures)
+{
+	
+}
+
+void OpenGLRenderer::update()
+{
+}
+
+void OpenGLRenderer::setCamera(Camera* newCamera)
+{
+	check(newCamera);
+
+	currentCamera = newCamera;
+
+}
+
+Camera* OpenGLRenderer::getCamera()
+{
+	return currentCamera;
 }
 
 extern "C" OpenGLRendererPlugin_API void registerModule(ModuleManager& mm)
