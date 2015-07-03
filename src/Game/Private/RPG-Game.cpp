e215d21cf6f2be10cab222a7d6c4c7f9a12d6ec8
Russell Greene <russellgreene8@gmail.com>
2015-07-03 16:30:30 -0600

    add the extra .dds file, and cleanup RPG-Game.cpp

--- src/Game/Private/RPG-Game.cpp
+++ src/Game/Private/RPG-Game.cpp
@@ -1,34 +1,8 @@
-// local includes
-#include "Engine.h"
-
-#include "Runtime.h"
-#include "Color.h"
-
-#include <map>
-#include <string>
-
-#include <boost/serialization/map.hpp>
-#include <boost/archive/xml_oarchive.hpp>
+#include <Runtime.h>
 
 int32 main()
 {	
-	//{
 
-	//	std::map<Color, std::string> assoc =
-	//	{
-	//		{ Color{ 0ui8, 0ui8, 0ui8, 255ui8 }, std::string{ "4" } },
-	//		{ Color{ 0ui8, 255ui8, 0ui8, 255ui8 }, std::string{ "spiral" } },
-	//		{ Color{ 255ui8, 255ui8, 255ui8, 255ui8 }, std::string{ "12" } },
-	//		{ Color{ 255ui8, 0ui8, 0ui8, 255ui8 }, std::string{ "8" } }
-
-	//	};
-
-	//	std::ofstream o_str{ "test.txt" };
-	//	boost::archive::xml_oarchive o_arch{ o_str };
-
-	//	o_arch << BOOST_SERIALIZATION_NVP(assoc);
-
-	//}
 
 	Runtime* runtime = new Runtime("default");
 
@@ -39,8 +13,6 @@ int32 main()
 	}
 	catch (ENGException& e)
 	{
-		std::string reason = e.what();
-		logger<Error>() << reason;
 		return -1;
 	}
 	catch (std::exception& e)
