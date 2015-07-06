d42b4951781da209925a7422422b86c422f931f8
Russell Greene <russellgreene8@gmail.com>
2015-07-06 17:50:28 -0600

    rrrrandom spawn points

--- src/TestContent/Private/Pew.cpp
+++ src/TestContent/Private/Pew.cpp
@@ -5,7 +5,7 @@ void Pew::tick(float deltaTime)
 	timeToSpawn -= deltaTime;
 	if (timeToSpawn <= 0) {
 		Gate* g = new Gate();
-		g->addWorldLocation(vec2(1.f, 1.f) );
-		timeToSpawn = 1.f;
+		g->addWorldLocation(vec2(rand() % 10, rand() % 10) );
+		timeToSpawn = .1f;
 	}
 }
